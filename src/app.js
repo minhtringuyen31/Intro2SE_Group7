a00cae6b2a1518b848c599302c129cb8b5601dd9
hhman24 <anhoang483@gmail.com>
2022-12-07 18:36:43 +0700

    hhman-10 update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 // use Module Morgan 
 app.use(morgan('tiny'));
+
+// use helmet
+app.use(helmet());
+
+// use cors
+app.use(cors());
 
 app.get('/', (req, res) => {
     return res.send('Hello word');
 });
 
 app.listen(port, () => console.log(`Listening to hhman port http://localhost:${port}`));
+
+
+// What api ?
+
+/**
+ * Folder Structure:
+ * src:
+ *  Router
+ *  Controller:
+ *  Service:
+ *  Model
+ *  Content-path:
+ *      api
+ *  Document: 
+ */
 
 
