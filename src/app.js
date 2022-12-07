3bc69764be85bec3da32b14d3b30f9a2ee8de93f
hhman24 <anhoang483@gmail.com>
2022-12-07 18:15:33 +0700

    hhman-8 use morgan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 const port = process.env.PORT || 3000;
+
+// use Module Morgan 
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     return res.send('Hello word');
