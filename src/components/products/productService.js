75c9d1a434c17b0bf0ac71381a7bd8249c7b2043
hhman24 <anhoang483@gmail.com>
2022-12-18 22:58:02 +0700

    hhman-21 update product get all, data

--- src/components/products/productService.js
+++ src/components/products/productService.js
@@ -1,19 +1,5 @@
 const product = require('./productModel');
 
-// lấy dữ liệu
-/**
- * async()
- * arrow function
- * exports = : tham chiếu trong một cùng nhớ mới
- * exports. : bổ sung phương thức
- * module.exports vs export cùng trỏ vào một vùng nhớ
- * promise: --> xử lý các thao tác bất đồng bộ
- *  - Sync: đồng bộ  chạy theo luồng
- *  - Async: không theo luồng
- *  - callback hell: 
- * js: đơn luồng, nhưng vẫn xử lý đa luồng
- */
-
 
 exports.getAllProduct = async() =>{
     const products = await product.find({});
