000933babebc74d5ae6902704f721f8765bbafbc
hhman24 <anhoang483@gmail.comC:/Users/hhman/AppData/Local/Programs/Python/Python39/python.exe>
2023-01-01 13:55:27 +0700

    hhman delete my branch to test new version

--- src/components/products/productRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('./productController');
-
-// router.get('/', function (req, res) {
-//     res.
-// })
-
-router.get('/', productController.getAllProduct);
--- src/components/products/productService.js
+++
@@ -1,7 +0,0 @@
-const product = require('./productModel');
-
-
-exports.getAllProduct = async() =>{
-    const products = await product.find({});
-    return products;
-}
