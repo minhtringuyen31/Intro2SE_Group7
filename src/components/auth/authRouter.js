d99c5521150568d07064debddd33b1607e95d4bb
Nguyen Vo Minh Tri <minhtri.nguyenvo31@gmail.com>
2023-01-06 21:28:38 +0700

    new 2

---
+++ src/components/auth/authRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('./authController');
+const { authenticate } = require('./passport');
+const passport = require('./passport');
+
+router.get('/sign', function (req, res, next) {
+  res.render('auth/sign', { layout: false });
+});
+
+router.get('/logout', authController.logout);
+
+router.get('/editProfile', function (req, res) {
+  res.render('auth/profiles');
+})
+
+router.post('/signin', passport.authenticate('local', {
+  successRedirect: '/index',
+  failureRedirect: '/auth/sign'
+}));
+
+router.post('/signup', authController.signup);
+
+router.get('/verify-email/:email', authController.verifyEmail);
+
+module.exports = router;
