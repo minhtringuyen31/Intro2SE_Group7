6e4a63f35db7878f0e76f61298d07582a60730bc
Minh Tri Nguyen Vo <89735910+minhtringuyen31@users.noreply.github.com>
2023-01-06 21:15:55 +0700

    Delete src directory

--- src/components/products/productRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('./productController');
-
-router.get('/', productController.productList);
-
-
-module.exports = router;
